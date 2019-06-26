3a4f8952e0ba33a79d6473468c21355b9a4a6e0d
gloryfrs <gloryfrs@yandex.ru>
2019-06-26 10:03:09 +0700

    Updates

---
+++ precache-manifest.e836f61f2120f2813f5a8988059f27d7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-constructor/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "aafeb23794c0546aa78e6d11e49ef908",
+    "url": "/vk-apps-constructor/static/media/GothaProReg.aafeb237.otf"
+  },
+  {
+    "revision": "dd3d9ca53a246950706e94726bb1403e",
+    "url": "/vk-apps-constructor/static/media/GothaProMed.dd3d9ca5.otf"
+  },
+  {
+    "revision": "3f9c5a2b33fd28799ac8e55c093a4531",
+    "url": "/vk-apps-constructor/static/media/GothaProBol.3f9c5a2b.otf"
+  },
+  {
+    "revision": "498631657658e2a8c693",
+    "url": "/vk-apps-constructor/static/js/runtime~main.eabfb822.js"
+  },
+  {
+    "revision": "0a17375b7f3ce2f6b5f8",
+    "url": "/vk-apps-constructor/static/js/main.2b0c940f.chunk.js"
+  },
+  {
+    "revision": "999c4066392bf36aa8d1",
+    "url": "/vk-apps-constructor/static/js/2.4d5aed10.chunk.js"
+  },
+  {
+    "revision": "0a17375b7f3ce2f6b5f8",
+    "url": "/vk-apps-constructor/static/css/main.39679e9c.chunk.css"
+  },
+  {
+    "revision": "999c4066392bf36aa8d1",
+    "url": "/vk-apps-constructor/static/css/2.d83faf9b.chunk.css"
+  },
+  {
+    "revision": "73fa75671a258508de0abf75ec88d7ce",
+    "url": "/vk-apps-constructor/index.html"
+  }
+];
